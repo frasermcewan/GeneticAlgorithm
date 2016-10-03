adf9591db0f516ce4b290b946c329fdafcd339f5
frasermcewan <qwb12173@uni.strath.ac.uk>
2016-10-04 00:16:45 +0100

    Formatting Changes

--- src/Collection.java
+++ src/Collection.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Collection {
 
-	int tournamentSize =5;
+	int tournamentSize = 5;
 	int collectionSize = 10000;
 	double mutationRatio = 0.02;
 	boolean increaseMutation = false;
@@ -12,14 +12,14 @@ public class Collection {
 	String targetString = "Hello, world!";
 	Random random = new Random();
 	GeneticAlg[] arrayCollection;
-	
+
 	public Collection() {
 		this.arrayCollection = generateCollection();
-		
+
 	}
-		
+
 	public GeneticAlg[] generateCollection() {
-		GeneticAlg [] temp;
+		GeneticAlg[] temp;
 		temp = new GeneticAlg[collectionSize];
 		for (int i = 0; i < collectionSize; i++) {
 			temp[i] = generateStrings();
@@ -27,39 +27,35 @@ public class Collection {
 		Arrays.sort(temp);
 		return temp;
 	}
-	
-	
-	
-	
+
 	public void naturalSelection() {
 		GeneticAlg[] temporaryList = new GeneticAlg[arrayCollection.length];
 		int elitismPoint = (int) Math.round(arrayCollection.length * elitismRatio);
 		for (int i = 0; i <= elitismPoint; i++) {
 			temporaryList[i] = arrayCollection[i];
-			
+
 		}
-		
+
 		while (elitismPoint < temporaryList.length) {
-			
-			
-			if(getFittest().fitness <=5 && increaseMutation == false) {
+
+			if (getFittest().fitness <= 5 && increaseMutation == false) {
 				mutationRatio = mutationRatio * 10;
 				increaseMutation = true;
 			}
-			
+
 			if (random.nextDouble() <= selectionRatio) {
-				
+
 				GeneticAlg[] parents = new GeneticAlg[2];
-				parents[0] = selectParent();
-				parents[1] = selectParent();
+				parents[0] = tournament();
+				parents[1] = tournament();
 				GeneticAlg[] children = parents[0].crossover(parents[1]);
-				
+
 				if (random.nextDouble() <= mutationRatio) {
 					temporaryList[elitismPoint++] = children[0].mutation();
 				} else {
-					temporaryList[ elitismPoint++] = children[0];
+					temporaryList[elitismPoint++] = children[0];
 				}
-				
+
 				if (elitismPoint < temporaryList.length) {
 					if (random.nextDouble() <= mutationRatio) {
 						temporaryList[elitismPoint] = children[1].mutation();
@@ -74,25 +70,24 @@ public class Collection {
 					temporaryList[elitismPoint] = arrayCollection[elitismPoint];
 				}
 			}
-	
-			++ elitismPoint;
+
+			++elitismPoint;
 		}
 
 		Arrays.sort(temporaryList);
 		arrayCollection = temporaryList;
 
-		
 	}
 
 	public GeneticAlg getFittest() {
 		return arrayCollection[0];
 	}
-	
+
 	public int Fitness() {
 		return arrayCollection[0].fitness;
-		
+
 	}
-	
+
 	public GeneticAlg generateStrings() {
 		char[] newCharacterArray = new char[targetString.length()];
 		for (int i = 0; i < newCharacterArray.length; i++) {
@@ -101,20 +96,17 @@ public class Collection {
 		return new GeneticAlg(String.valueOf(newCharacterArray));
 	}
 
-		
-	private GeneticAlg selectParent() {
-		    GeneticAlg parent;
-			parent = arrayCollection[random.nextInt(arrayCollection.length)];
-			for (int j = 0; j < tournamentSize; j++) {
-				int point = random.nextInt(arrayCollection.length);
-				if (arrayCollection[point].compareTo(parent) < 0) {
-					parent = arrayCollection[point];
+	private GeneticAlg tournament() {
+		GeneticAlg parent;
+		parent = arrayCollection[random.nextInt(arrayCollection.length)];
+		for (int j = 0; j < tournamentSize; j++) {
+			int point = random.nextInt(arrayCollection.length);
+			if (arrayCollection[point].compareTo(parent) < 0) {
+				parent = arrayCollection[point];
 			}
 		}
-		
+
 		return parent;
 	}
-	
-	
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,30 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Collection col = new Collection();
 		GeneticAlg alpha = col.getFittest();
 		int i = 0;
 		int j = 0;
 
 		while (alpha.returnFitness() != 0) {
-			System.out.println(alpha.getVersion() + ' ' +  i + " Fitness  " + alpha.fitness );
+			System.out.println(alpha.getVersion() + ' ' + i + " Fitness  " + alpha.fitness);
 			col.naturalSelection();
 			alpha = col.getFittest();
 			i++;
 		}
 
 		System.out.println("Final Version " + i + ": " + alpha.getVersion() + "\n");
-//		System.out.println("Starting Random Analysis \n\n\n");
-//		GeneticAlg zeta = col.generateStrings();
-//
-//		while(j <= 10000  && (!zeta.equals("Hello, world!"))) {
-//			System.out.println(zeta.getVersion() + ' ' +  i + " Fitness  " + zeta.fitness + " " + j);
-//			zeta = col.generateStrings();
-//			j++;
-//		}
-		
+		// System.out.println("Starting Random Analysis \n\n\n");
+		// GeneticAlg zeta = col.generateStrings();
+		//
+		// while(j <= 10000 && (!zeta.equals("Hello, world!"))) {
+		// System.out.println(zeta.getVersion() + ' ' + i + " Fitness " +
+		// zeta.fitness + " " + j);
+		// zeta = col.generateStrings();
+		// j++;
+		// }
+
 	}
 
 }
