db99ec5a2573fbdd840098590563b96f68cfab12
frasermcewan <qwb12173@uni.strath.ac.uk>
2016-10-04 11:34:55 +0100

    Added Commentary

--- src/Collection.java
+++ src/Collection.java
@@ -3,10 +3,20 @@ import java.util.Random;
 
 public class Collection {
 
+	/**
+	 * Collection class creates the population of different strains, carries out the evolution process and runs the tournament
+	 * in order to determine what parents we should be taking forward into the next generation. This class also uses elitism
+	 * to copy a set amount of the better strains over to the next generation to ensure that we reach the target quicker. An 
+	 * instance of this class is created within the Main class. Below are the variables defining the sizes and rates for the
+	 * evolution process.
+	 *
+	 */
+	
 	int tournamentSize = 5;
-	int collectionSize = 10000;
+	int collectionSize = 1000;
 	double mutationRatio = 0.02;
 	boolean increaseMutation = false;
+	boolean increaseMutation2 = false;
 	double selectionRatio = 0.85;
 	double elitismRatio = 0.2;
 	String targetString = "Hello, world!";
@@ -14,21 +24,35 @@ public class Collection {
 	GeneticAlg[] arrayCollection;
 
 	public Collection() {
+		/**
+		 * Class instance calls the generate collection method which populates the array arrayCollection with GeneticAlg
+		 * class objects. These object contain the fitness of that object as well as the String that the object currently has
+		 * in the form of a char array.
+		 */
 		this.arrayCollection = generateCollection();
 
 	}
 
 	public GeneticAlg[] generateCollection() {
+		/** 
+		 * This method creates a temporary array of the population and passes it back to be stored in the arrayCollection
+		 * array, it calls the generate Strings method which populates the string within the object
+		 */
 		GeneticAlg[] temp;
 		temp = new GeneticAlg[collectionSize];
 		for (int i = 0; i < collectionSize; i++) {
 			temp[i] = generateStrings();
 		}
-		Arrays.sort(temp);
+		
 		return temp;
 	}
 
 	public void naturalSelection() {
+		/**
+		 * The evolution process creates a temporary array of GeneticAlgs, identifies an elitism point and copies the
+		 * best versions over to the temporary array so that we have a good population for the next generation.
+		 */
+		
 		GeneticAlg[] temporaryList = new GeneticAlg[arrayCollection.length];
 		int elitismPoint = (int) Math.round(arrayCollection.length * elitismRatio);
 		for (int i = 0; i <= elitismPoint; i++) {
@@ -36,59 +60,126 @@ public class Collection {
 
 		}
 
+		/**
+		 * While there is still strains to be copied
+		 */
+		
 		while (elitismPoint < temporaryList.length) {
-
+/**
+ * Asses the best fitness, if fitness is <=5 increase the mutation to try and make the small changes needed
+ * to reach the goal, however only do this once as we don't want mutation smoothing the over factors too much and becoming
+ * too significant as too much mutation would lead us away from the target.
+ */
+			
+			
+			
 			if (getFittest().fitness <= 5 && increaseMutation == false) {
 				mutationRatio = mutationRatio * 10;
 				increaseMutation = true;
 			}
+			
+			if (getFittest().fitness <=1 && increaseMutation2 == false) {
+				mutationRatio = mutationRatio * 25;
+				increaseMutation2 = true;
+			}
 
+			
+			/**
+			 * Checks to see if crossover should happen by comparing a random double to the rate set at the start of the class
+			 * This is done as crossover should only happen a percentage of the time and not always.
+			 */
+			
 			if (random.nextDouble() <= selectionRatio) {
 
-				GeneticAlg[] parents = new GeneticAlg[2];
-				parents[0] = tournament();
-				parents[1] = tournament();
-				GeneticAlg[] children = parents[0].crossover(parents[1]);
+				/**
+				 * If crossover is to happen, create a parents list, call our tournament two times in order to get the two 
+				 * strains that are most suited to be parents, then crossover those parents using the crossover method
+				 * Available in the GeneticAlg class which will then populate a children array which we then check to see
+				 * if mutation should happen against.
+				 */
+				
+				GeneticAlg[] parentVersions = new GeneticAlg[2];
+				parentVersions[0] = tournament();
+				parentVersions[1] = tournament();
+				GeneticAlg[] childrenVersions = parentVersions[0].crossover(parentVersions[1]);
 
 				if (random.nextDouble() <= mutationRatio) {
-					temporaryList[elitismPoint++] = children[0].mutation();
+					/**
+					 * If the next double indicates mutation should happen when checked against the mutationRatio
+					 * then mutate the first childVersion that we have and add it to the temporaryList of the next
+					 * generation, else mutation should not happen and it gets added to the temporaryList
+					 */
+					
+					temporaryList[elitismPoint++] = childrenVersions[0].mutation();
 				} else {
-					temporaryList[elitismPoint++] = children[0];
+					temporaryList[elitismPoint++] = childrenVersions[0];
 				}
 
 				if (elitismPoint < temporaryList.length) {
 					if (random.nextDouble() <= mutationRatio) {
-						temporaryList[elitismPoint] = children[1].mutation();
+						/**
+						 * If the next double indicates mutation should happen when checked against the mutationRatio
+						 * then mutate the second childVersion that we have and add it to the temporaryList of the next
+						 * generation, else mutation should not happen and it gets added to the temporaryList
+						 */
+						
+						temporaryList[elitismPoint] = childrenVersions[1].mutation();
 					} else {
-						temporaryList[elitismPoint] = children[1];
+						temporaryList[elitismPoint] = childrenVersions[1];
 					}
 				}
-			} else {
+				
+			} 
+			/**
+			 * If crossover should not happen, then we do the mutation check as the are not mutually inclusive, check to see
+			 * if the next double results in a double that will trigger mutation, if so mutate the GeneticAlg assigned at
+			 * the elitismPoint and then add it to the temporaryList, however the mutationRatio may not trigger and the 
+			 * else statement will assign it to the temporaryList as normal
+			 */
+			
+			else {
 				if (random.nextDouble() <= mutationRatio) {
 					temporaryList[elitismPoint] = arrayCollection[elitismPoint].mutation();
 				} else {
 					temporaryList[elitismPoint] = arrayCollection[elitismPoint];
 				}
 			}
-
+			/**
+			 * Iterating the point forward
+			 */
 			++elitismPoint;
 		}
 
+		
+		/**
+		 * Arrays.Sort compares the list and then sorts it in descending order of fitness, it then moves the tempList
+		 * into the arrayCollection
+		 */
 		Arrays.sort(temporaryList);
 		arrayCollection = temporaryList;
 
 	}
 
 	public GeneticAlg getFittest() {
+		/**
+		 * returns the alpha version in order to determine how close we are to the Target
+		 */
 		return arrayCollection[0];
 	}
 
 	public int Fitness() {
+		/**
+		 * Returns the fitness of the alpha version
+		 */
 		return arrayCollection[0].fitness;
 
 	}
 
 	public GeneticAlg generateStrings() {
+		/**
+		 * Generates strings by by creating a character array and population the array with random ASCII values
+		 * generated by the random. The strings are of fixed length to the target string to avoid complications.
+		 */
 		char[] newCharacterArray = new char[targetString.length()];
 		for (int i = 0; i < newCharacterArray.length; i++) {
 			newCharacterArray[i] = (char) (random.nextInt(90) + 32);
@@ -97,6 +188,14 @@ public class Collection {
 	}
 
 	private GeneticAlg tournament() {
+		/**
+		 * Creates a tournament which gets called twice in order to determine the fittest parent to take forward, takes a
+		 * random point within the arrayCollection and assigns it as the best, then takes another point and then checks in a 
+		 * for loop up to the tournament size if the point that it has is the fitter version than the parent it already has, and
+		 * if it is then assign that version to be the parent. It then returns the parent which is used in the evolution stage 
+		 * to perform crossover
+		 */
+		
 		GeneticAlg parent;
 		parent = arrayCollection[random.nextInt(arrayCollection.length)];
 		for (int j = 0; j < tournamentSize; j++) {
--- src/GeneticAlg.java
+++ src/GeneticAlg.java
@@ -1,5 +1,14 @@
 import java.util.Random;
 
+/**
+ * 
+ * This class is used as an object class which implements Comparable in order to allow it to be compared
+ * against other objects of the same type. This is used to determine which object is the fitter when compared against
+ * each other. The object takes in a String which is then compared to the targetString and the object is assigned a fitness
+ * score. The class also contains methods that allow mutation, crossover and fitness comparison
+ *
+ */
+
 public class GeneticAlg implements Comparable<GeneticAlg> {
 	Random random = new Random();
 	char[] chromoIn;
@@ -8,16 +17,26 @@ public class GeneticAlg implements Comparable<GeneticAlg> {
 	char[] targetStringinArray = "Hello, world!".toCharArray();
 
 	public GeneticAlg(String initialString) {
+		/**
+		 * Takes in the string and calls the fitness function to determine how close it is to target
+		 */
 		this.stringLength = initialString.length();
 		this.chromoIn = toCharArray(initialString);
 		setFitness(chromoIn);
 	}
 
 	public String getVersion() {
+		/**
+		 * Accessory method in order to return the String value of the current char array
+		 */
 		return String.valueOf(chromoIn);
 	}
 
 	public void setFitness(char[] chromo) {
+		/**
+		 * Called by instance of the class, works out the fitness of the Object by comparing the 
+		 * numerical ASCII values against each other. Maths.abs is used to ensure non negative values
+		 */
 		for (int i = 0; i < chromo.length; i++) {
 			fitness += Math.abs(((int) chromo[i]) - ((int) targetStringinArray[i]));
 		}
@@ -25,16 +44,29 @@ public class GeneticAlg implements Comparable<GeneticAlg> {
 	}
 
 	public int returnFitness() {
+		/**
+		 * Accessory method to return fitness 
+		 */
 		return fitness;
 	}
 
 	public char[] toCharArray(String In) {
+		/**
+		 * Converts the string to a character array for use within the object class, is used as part of determining 
+		 * fitness of the object
+		 */
 		char[] convertorArray;
 		convertorArray = In.toCharArray();
 		return convertorArray;
 	}
 
 	public GeneticAlg mutation() {
+		/**
+		 * Method for mutation, takes the current char array and stores it in a temporary char array. Then takes a random
+		 * point of length of that character array. Then creates a new int value of a random ASCII character and then assigns
+		 * the char value to that random point within the temporary Array and returns the updated mutated version in the form
+		 * of a new Object
+		 */
 		char[] mutArray = chromoIn;
 		int randomPoint = random.nextInt(mutArray.length);
 		int newValue = (random.nextInt(90) + 32);
@@ -44,12 +76,25 @@ public class GeneticAlg implements Comparable<GeneticAlg> {
 	}
 
 	public GeneticAlg[] crossover(GeneticAlg partner) {
+		/**
+		 * Takes in another Object of Genetic Alg and stores its own String and the other objects string in a char array.
+		 * It then works out a random pivotPoint and creates two children Arrays of the same length as the parents in order
+		 * to store the crossover.
+		 * 
+		 */
 		char[] parent1 = chromoIn;
 		char[] parent2 = partner.chromoIn;
 		int pivotPoint = random.nextInt(parent1.length);
 		char[] child1 = new char[stringLength];
 		char[] child2 = new char[stringLength];
 
+		/**
+		 * For everything up to and including the pivotPoint, populate the children with the characters of parents 1/2 for
+		 * children 1/2 and then repeat the reverse for the second child. I rather stupidly started my second for loop with
+		 * j=0 which wiped out the first portion of each child, however fixed this problem and it helped improve my number of
+		 * generations by the tens of thousands! 
+		 */
+		
 		for (int i = 0; i <= pivotPoint; i++) {
 			child1[i] = parent1[i];
 			child2[i] = parent2[i];
@@ -61,18 +106,26 @@ public class GeneticAlg implements Comparable<GeneticAlg> {
 			child2[j] = parent1[j];
 		}
 
+		/**
+		 * Return the two children as new instances of GeneticAlg objects which are used in the evolution stage
+		 * to add to the population
+		 */
 		return new GeneticAlg[] { new GeneticAlg(String.valueOf(child1)), new GeneticAlg(String.valueOf(child2)) };
 	}
 
 	@Override
 	public int compareTo(GeneticAlg o) {
+		/**
+		 * Implementation of compare to which is part of the implementation of Comparable. This allows different objects
+		 * to compare themselves and determine which one has the best fitness in order to choose that as a potential parent
+		 */
 		if (fitness < o.fitness) {
 			return -1;
 		} else if (fitness > o.fitness) {
 			return 1;
-		}
-
+		} else {
 		return 0;
+		}
 
 	}
 
