c864b71a979b5a09e48a45a61976738e48a0d366
frasermcewan <qwb12173@uni.strath.ac.uk>
2016-10-04 11:43:47 +0100

    More comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
+		/**
+		 * Creates a new population, gets the fittest version and then evolves the population while the fitness does
+		 * not equal zero while printing out the alpha version and its level of fitness as it procedes through evolution
+		 * layers. With the current parameters set in the population class is takes between 50 and 110 evolutions to reach
+		 * the target
+		 */
 
 		Collection col = new Collection();
 		GeneticAlg alpha = col.getFittest();
@@ -16,10 +23,18 @@ public class Main {
 		}
 
 		System.out.println("Final Version " + i + ": " + alpha.getVersion() + "\n");
+		
+		
+		
+		/**
+		 * Below is the code to implement a random String generation in order to see how long it would take to randomly
+		 * get Hello, world!, I set the limit for it at 100,000 attempts but as of yet it fails to reach this.
+		 */
+		
 		// System.out.println("Starting Random Analysis \n\n\n");
 		// GeneticAlg zeta = col.generateStrings();
 		//
-		// while(j <= 10000 && (!zeta.equals("Hello, world!"))) {
+		// while(j <= 100000 && (!zeta.equals("Hello, world!"))) {
 		// System.out.println(zeta.getVersion() + ' ' + i + " Fitness " +
 		// zeta.fitness + " " + j);
 		// zeta = col.generateStrings();
